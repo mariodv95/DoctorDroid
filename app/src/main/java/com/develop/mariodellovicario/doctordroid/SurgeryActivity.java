f3653300f20c1ba822c6e114147bd21ef7eff2cd
mariodv95 <mario.dellovicario@gmail.com>
2018-03-27 18:58:57 +0200

    3

--- app/src/main/java/com/develop/mariodellovicario/doctordroid/SurgeryActivity.java
+++ app/src/main/java/com/develop/mariodellovicario/doctordroid/SurgeryActivity.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -25,6 +28,25 @@ public class SurgeryActivity extends AppCompatActivity {
     Button infContBtn;
     Button oppPerBtn;
     Button oppSubBtn;
+
+
+    //blocco toolbar inizio
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        Toast.makeText(getApplicationContext(), "info", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+    //blocco toolbar fine
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +67,7 @@ public class SurgeryActivity extends AppCompatActivity {
 
         // Listview Group click listener
         expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
 
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v,
