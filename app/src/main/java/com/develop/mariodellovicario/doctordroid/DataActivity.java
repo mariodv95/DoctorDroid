62d631bf26564e54aceee8d8c412ce7176223408
mariodv95 <mario.dellovicario@gmail.com>
2018-04-10 18:49:20 +0200

    11

--- app/src/main/java/com/develop/mariodellovicario/doctordroid/DataActivity.java
+++ app/src/main/java/com/develop/mariodellovicario/doctordroid/DataActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class DataActivity extends AppCompatActivity {
+
+    Button mostraTabellaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,21 @@ public class DataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_data);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //BOTTONE PER MOSTRARE TABELLA /////////////////////
+
+        mostraTabellaBtn = (Button) findViewById(R.id.btnDataActivity);
+
+        mostraTabellaBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //thigs to do on button press...example toast:
+                Toast.makeText(getApplicationContext(), "Mostra Tabella Corrispondente", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /////////////////////////////////////////////////
     }
 
 }
