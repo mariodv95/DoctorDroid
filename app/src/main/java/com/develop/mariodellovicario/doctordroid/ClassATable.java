95d2b65e170fa86356a0d288d0799263fd53a313
mariodv95 <mario.dellovicario@gmail.com>
2018-09-15 12:26:58 +0200

    32

--- app/src/main/java/com/develop/mariodellovicario/doctordroid/ClassATable.java
+++ app/src/main/java/com/develop/mariodellovicario/doctordroid/ClassATable.java
@@ -54,6 +54,14 @@ public class ClassATable extends AppCompatActivity {
             farmSalv.setText("Ketoralac 10mg/12h  || Ketoprofene 100mg/12h");
             gastrSalv.setText("Pantoprazolo 40mg");
             //DOPO 24 ORE
+            protoDp24.setText("A3 dopo 24 ore - il paziente si alimenta");
+            sommDp24.setText("os");
+            farmDp24.setText("Paracetamolo 1g/8h");
+            gastrDp24.setText("No");
+            protoSalvDp24.setText("Operazioni terapeutiche di salvataggio da associare alla precedente se NRS > 3");
+            sommSalvDp24.setText("os");
+            farmSalvDp24.setText("Tramadolo gtt (100 mg/ml) 50-100 mg (equivalenti a 20-40 gtt) + Metoclopramide 10 mg\nIbuprofene 400-600 mg \nParacetamolo 500 mg + Codeina 30 mg (sospendere terapia con paracetamolo)  1-2 cpr o bustine (max 6 cpr o bustine/die)");
+            gastrSalvDp24.setText("Pantoprazolo 40 mg/die (solo per Ibuprofene)\n");
 
         }else if( alim.equals("siAlim") && fans.equals("intollerante")){
             //PRIME 24 ORE
@@ -67,6 +75,14 @@ public class ClassATable extends AppCompatActivity {
                     "Paracetamolo 500mg + Codeina 30mg(sospendere terapia con paracetamolo) 1-2 cpr o bustine/6h(max 6 cp o bustine/die)");
             gastrSalv.setText("No");
             //DOPO 24 ORE
+            protoDp24.setText("A4 dopo 24 ore - il paziente si alimenta e FANS controindicati");
+            sommDp24.setText("os");
+            farmDp24.setText("Paracetamolo 1 g / 8h\n");
+            gastrDp24.setText("No");
+            protoSalvDp24.setText("Opzioni terapeutiche di salvataggio da associare alla precedente se NRS >3\n");
+            sommSalvDp24.setText("os\n");
+            farmSalvDp24.setText("Tramadolo gtt (100 mg/ml) 50-100 mg (equivalenti a 20-40 gtt) + Metoclopramide 10 mg\nParacetamolo 500 mg + Codeina 30 mg (sospendere terapia con paracetamolo)  1-2 cpr o bustine (max 6 cpr o bustine/die)\n");
+            gastrSalvDp24.setText("No");
 
         }else if( alim.equals("noAlim") && fans.equals("tollerante")){
             //PRIME 24 ORE
@@ -79,6 +95,14 @@ public class ClassATable extends AppCompatActivity {
             farmSalv.setText("Ketoralac 30mg ev in sol. fis. 100ml ripetibile ogni 8h");
             gastrSalv.setText("Pantoprazolo 40mg");
             //DOPO 24 ORE
+            protoDp24.setText("A1 dopo 24 ore- il paziente non si alimenta");
+            sommDp24.setText("ev\n");
+            farmDp24.setText("Paracetamolo 1 g in 15’ ogni 8h\n");
+            gastrDp24.setText("No");
+            protoSalvDp24.setText("Opzioni terapeutiche di salvataggio da associare alla precedente se NRS >3\n");
+            sommSalvDp24.setText("ev\n");
+            farmSalvDp24.setText("Tramadolo 50-100 mg in Sol.Fis.100 ml in 30’ + Metoclopramide 10 mg\nKetoprofene 160 mg in Sol.Fis.100 ml in 15'\n");
+            gastrSalvDp24.setText("Pantoprazolo 40 mg/die\n (solo per Ketoprofene)");
 
         }else if( alim.equals("noAlim") && fans.equals("tollerante") && eta.equals("piu65")){
             //PRIME 24 ORE
@@ -91,6 +115,14 @@ public class ClassATable extends AppCompatActivity {
             farmSalv.setText("Ketoralac 30mg ev in sol. fis. 100ml ripetibile ogni 12h");
             gastrSalv.setText("Pantoprazolo 40mg");
             //DOPO 24 ORE
+            protoDp24.setText("A1 dopo 24 ore- il paziente non si alimenta");
+            sommDp24.setText("ev\n");
+            farmDp24.setText("Paracetamolo 1 g in 15’ ogni 8h\n");
+            gastrDp24.setText("No");
+            protoSalvDp24.setText("Opzioni terapeutiche di salvataggio da associare alla precedente se NRS >3\n");
+            sommSalvDp24.setText("ev\n");
+            farmSalvDp24.setText("Tramadolo 50-100 mg in Sol.Fis.100 ml in 30’ + Metoclopramide 10 mg\nKetoprofene 160 mg in Sol.Fis.100 ml in 15'\n");
+            gastrSalvDp24.setText("Pantoprazolo 40 mg/die\n (solo per Ketoprofene)");
 
         }else if( alim.equals("noAlim") && fans.equals("intollerante")){
             //PRIME 24 ORE
@@ -103,6 +135,14 @@ public class ClassATable extends AppCompatActivity {
             farmSalv.setText("Tramadolo 10mg in 100ml di sol. fis. in 30' + Metoclopramide 10mg");
             gastrSalv.setText("No");
             //DOPO 24 ORE
+            protoDp24.setText("A2 dopo 24 ore - il paziente non si alimenta e FANS controindicati");
+            sommDp24.setText("ev\n");
+            farmDp24.setText("Paracetamolo 1 g in 15’ ogni 8h\n");
+            gastrDp24.setText("No");
+            protoSalvDp24.setText("Opzioni terapeutiche di salvataggio da associare alla precedente se NRS >3\n");
+            sommSalvDp24.setText("ev\n");
+            farmSalvDp24.setText("Tramadolo 50-100 mg in Sol.Fis.100 ml in 30’ + Metoclopramide 10 mg\n");
+            gastrSalvDp24.setText("No");
 
         }
     }
